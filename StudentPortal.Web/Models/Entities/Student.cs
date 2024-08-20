1b12bdf531d4a805ff57cc556974269bfd6442e3
Ahmad-Gurmani <ahmad.gurmani06@gmail.com>
2024-08-20 19:36:52 +0500

    Add project files.

---
+++ StudentPortal.Web/Models/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentPortal.Web.Models.Entities
+{
+    public class Student
+    {
+       // below are properties 
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public bool Subscribed { get; set; }
+    }
+}
