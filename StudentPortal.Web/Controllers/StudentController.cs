157acf9a069efd9ec3832f926e098de6b15719da
Ahmad-Gurmani <ahmad.gurmani06@gmail.com>
2024-08-21 01:04:22 +0500

    Add and read operations done in MVC crud

--- StudentPortal.Web/Controllers/StudentController.cs
+++ StudentPortal.Web/Controllers/StudentController.cs
@@ -1,13 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using StudentPortal.Web.Data;
+using StudentPortal.Web.Models;
+using StudentPortal.Web.Models.Entities;
 
 namespace StudentPortal.Web.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+        public StudentController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost] 
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed,
+            };
+
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> List()
+        {
+            var students = await dbContext.Students.ToListAsync();
+
+            return View(students);
         }
     }
 }
